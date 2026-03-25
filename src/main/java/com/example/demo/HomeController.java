78dae3495d45f6b9fee0a60f996bf341da37348f
Srinathr1207 <144497521+Srinathr1207@users.noreply.github.com>
2026-03-25 19:07:10 -0400

    Added HTML and Thymeleaf

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "Hello! This is my Java site running in the cloud.";
+        return "index";
     }
 
 }
